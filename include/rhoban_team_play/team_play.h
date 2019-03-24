15e1b11077187443ba5e3c4ecb05f2a23efd53f1
medrimonia <medrimonia@gmail.com>
2019-03-24 16:38:07 +0100

    clang auto-format based on ros coding-style

--- include/rhoban_team_play/team_play.h
+++ include/rhoban_team_play/team_play.h
@@ -5,16 +5,15 @@
 
 namespace rhoban_team_play
 {
- 
 // Broadcaster ports [UDP port]
-#define TEAM_PLAY_PORT  28645
-#define CAPTAIN_PORT    28646
+#define TEAM_PLAY_PORT 28645
+#define CAPTAIN_PORT 28646
 
 // Broadcaster frequencies [Hz]
-#define TEAM_PLAY_FREQUENCY     3
-#define CAPTAIN_FREQUENCY       5
+#define TEAM_PLAY_FREQUENCY 3
+#define CAPTAIN_FREQUENCY 5
 
-/// Maximal number of obstacles shared by each robot 
+/// Maximal number of obstacles shared by each robot
 #define MAX_OBSTACLES 10
 
 /// Maximal number of opponents considered in consensus
@@ -23,103 +22,107 @@ namespace rhoban_team_play
 /**
  * Robot playing state in teamplay
  */
-enum TeamPlayState : int {
-    Inactive = 0,
-    Playing = 1,
-    BallHandling = 2,
-    GoalKeeping = 8,
-    Unknown = 16
+enum TeamPlayState : int
+{
+  Inactive = 0,
+  Playing = 1,
+  BallHandling = 2,
+  GoalKeeping = 8,
+  Unknown = 16
 };
 
 /**
  * TeamPlay mMessage structure
  */
-struct TeamPlayInfo {
-    //ID of the player
-    int id;
-    //State of the player
-    TeamPlayState state;
-    //Ball position in self frame
-    float ballX, ballY, ballQ;
-    bool ballOk;
-    /// Ball speed along x-axis in self referential
-    float ballVelX;
-    /// Ball speed along y-axis in self referential
-    float ballVelY;
-    //Robot pose in field (fieldYaw is [rad])
-    float fieldX, fieldY, fieldYaw, fieldQ, fieldConsistency;
-    bool fieldOk;
-    // Placing target
-    bool placing;
-    float targetX, targetY;
-    float localTargetX, localTargetY;
-    // Ball target
-    float ballTargetX, ballTargetY;
-    /// Time elapsed since last kick was performed
-    float timeSinceLastKick;
-    //Referee textual state
-    char stateReferee[15];
-    //Robocup textual state
-    char stateRobocup[10];
-    //Playing textual state
-    char statePlaying[10];
-    //Approach textual state
-    char stateSearch[10];
-    //Hardware warnings
-    char hardwareWarnings[30];
-    //Robot clock
-    uint8_t hour, min, sec;
-    /// Obstacles
-    int nbObstacles;
-    float obstaclesRadius;
-    float obstacles[MAX_OBSTACLES][2];
+struct TeamPlayInfo
+{
+  // ID of the player
+  int id;
+  // State of the player
+  TeamPlayState state;
+  // Ball position in self frame
+  float ballX, ballY, ballQ;
+  bool ballOk;
+  /// Ball speed along x-axis in self referential
+  float ballVelX;
+  /// Ball speed along y-axis in self referential
+  float ballVelY;
+  // Robot pose in field (fieldYaw is [rad])
+  float fieldX, fieldY, fieldYaw, fieldQ, fieldConsistency;
+  bool fieldOk;
+  // Placing target
+  bool placing;
+  float targetX, targetY;
+  float localTargetX, localTargetY;
+  // Ball target
+  float ballTargetX, ballTargetY;
+  /// Time elapsed since last kick was performed
+  float timeSinceLastKick;
+  // Referee textual state
+  char stateReferee[15];
+  // Robocup textual state
+  char stateRobocup[10];
+  // Playing textual state
+  char statePlaying[10];
+  // Approach textual state
+  char stateSearch[10];
+  // Hardware warnings
+  char hardwareWarnings[30];
+  // Robot clock
+  uint8_t hour, min, sec;
+  /// Obstacles
+  int nbObstacles;
+  float obstaclesRadius;
+  float obstacles[MAX_OBSTACLES][2];
 
-    //Timestamp of data reception
-    //in milliseconds
-    float timestamp;
-    
-    // Is the player goal keeper ?
-    bool goalKeeper;
+  // Timestamp of data reception
+  // in milliseconds
+  float timestamp;
 
-    /**
-     * Return the time in milliseconds
-     * since data reception
-     */
-    float getAge() const;
+  // Is the player goal keeper ?
+  bool goalKeeper;
 
-    /**
-     * Return true if data have been
-     * received for too long
-     */
-    bool isOutdated() const;
+  /**
+   * Return the time in milliseconds
+   * since data reception
+   */
+  float getAge() const;
 
-    bool isPenalized() const;
+  /**
+   * Return true if data have been
+   * received for too long
+   */
+  bool isOutdated() const;
 
-    /**
-     * Return the distance and the
-     * azimuth between the robot
-     * and the ball
-     */
-    float getBallDistance() const;
-    float getBallAzimuth() const;
+  bool isPenalized() const;
 
-    /// Return the position of the ball in the field according to internal content
-    /// Warning: this value is meaningless if !(ballOk && fieldOk)
-    Eigen::Vector2d getBallInField() const;
+  /**
+   * Return the distance and the
+   * azimuth between the robot
+   * and the ball
+   */
+  float getBallDistance() const;
+  float getBallAzimuth() const;
+
+  /// Return the position of the ball in the field according to internal content
+  /// Warning: this value is meaningless if !(ballOk && fieldOk)
+  Eigen::Vector2d getBallInField() const;
 };
 
-void teamPlayfromJson(TeamPlayInfo &info, const Json::Value & json_value);
-Json::Value teamPlayToJson(const TeamPlayInfo &info);
+void teamPlayfromJson(TeamPlayInfo& info, const Json::Value& json_value);
+Json::Value teamPlayToJson(const TeamPlayInfo& info);
 
-#define CAPTAIN_MAX_ID  6
+#define CAPTAIN_MAX_ID 6
 
-enum CaptainOrder : int {
-    SearchBall = 0,
-    HandleBall = 1,
-    Place = 2
+enum CaptainOrder : int
+{
+  SearchBall = 0,
+  HandleBall = 1,
+  Place = 2
 };
 
-struct CommonBall {
+struct CommonBall
+{
   /// Number of robots agreeing on the Ball position
   int nbRobots;
   /// Pos on field [m]
@@ -128,7 +131,8 @@ struct CommonBall {
   float y;
 };
 
-struct CommonOpponent {
+struct CommonOpponent
+{
   /// Number of robots agreeing
   int consensusStrength;
   /// Pos on field [m]
@@ -139,39 +143,37 @@ struct CommonOpponent {
 
 struct CaptainInfo
 {
+  CaptainInfo();
 
-    CaptainInfo();
-    
-    // Captain id
-    int id;
-    // Targets position & orientation for robots
-    float robotTarget[CAPTAIN_MAX_ID][3];
-    // Captain instruction for each robot
-    CaptainOrder order[CAPTAIN_MAX_ID];
-    // Reception timestamp
-    float timestamp;
+  // Captain id
+  int id;
+  // Targets position & orientation for robots
+  float robotTarget[CAPTAIN_MAX_ID][3];
+  // Captain instruction for each robot
+  CaptainOrder order[CAPTAIN_MAX_ID];
+  // Reception timestamp
+  float timestamp;
 
-    /// Number of opponents in consensus
-    int nb_opponents;
-    /// List of the catkin opponents
-    CommonOpponent common_opponents[MAX_OPPONENTS];
+  /// Number of opponents in consensus
+  int nb_opponents;
+  /// List of the catkin opponents
+  CommonOpponent common_opponents[MAX_OPPONENTS];
 
-    CommonBall common_ball;
-    
-    /**
-     * Return the time in milliseconds
-     * since data reception
-     */
-    float getAge() const;
+  CommonBall common_ball;
 
-    /// Return the id of the robot handling the ball, if none of the robot is
-    /// handling the ball, return -1
-    /// Warning: ids start at '1'
-    int getHandler() const;
-    
+  /**
+   * Return the time in milliseconds
+   * since data reception
+   */
+  float getAge() const;
+
+  /// Return the id of the robot handling the ball, if none of the robot is
+  /// handling the ball, return -1
+  /// Warning: ids start at '1'
+  int getHandler() const;
 };
 
-void captainFromJson(CaptainInfo &info, const Json::Value & json_value);
-Json::Value captainToJson(const CaptainInfo &info);
+void captainFromJson(CaptainInfo& info, const Json::Value& json_value);
+Json::Value captainToJson(const CaptainInfo& info);
 
-}
+}  // namespace rhoban_team_play
--- src/rhoban_team_play/team_play.cpp
+++ src/rhoban_team_play/team_play.cpp
@@ -9,144 +9,148 @@ using namespace rhoban_utils;
 
 namespace rhoban_team_play
 {
+void teamPlayfromJson(TeamPlayInfo& info, const Json::Value& json_value)
+{
+  if (json_value.size() >= 33)
+  {
+    int k = 0;
 
-void teamPlayfromJson(TeamPlayInfo &info, const Json::Value & json_value)
-{
-    if (json_value.size() >= 33) {
-        int k = 0;
-        
-        // Robot id and state
-        info.id = json_value[k++].asInt();
-        info.state = (TeamPlayState)json_value[k++].asInt();
-        
-        // Ball
-        info.ballX = json_value[k++].asFloat();
-        info.ballY = json_value[k++].asFloat();
-        info.ballQ = json_value[k++].asFloat();
-        info.ballOk = json_value[k++].asBool();
-        info.ballVelX = json_value[k++].asFloat();
-        info.ballVelY = json_value[k++].asFloat();
-        
-        // Field
-        info.fieldX = json_value[k++].asFloat();
-        info.fieldY = json_value[k++].asFloat();
-        info.fieldYaw = json_value[k++].asFloat();
-        info.fieldQ = json_value[k++].asFloat();
-        info.fieldConsistency = json_value[k++].asFloat();
-        info.fieldOk = json_value[k++].asBool();
-        
-        // Placing data
-        info.placing = json_value[k++].asBool();
-        info.targetX = json_value[k++].asFloat();
-        info.targetY = json_value[k++].asFloat();
-        info.localTargetX = json_value[k++].asFloat();
-        info.localTargetY = json_value[k++].asFloat();
-        
-        // Kick
-        info.ballTargetX = json_value[k++].asFloat();
-        info.ballTargetY = json_value[k++].asFloat();
-        info.timeSinceLastKick = json_value[k++].asFloat();
-        
-        // States
-        strncpy(info.stateReferee, json_value[k++].asString().c_str(), sizeof(info.stateReferee));
-        strncpy(info.stateRobocup, json_value[k++].asString().c_str(), sizeof(info.stateRobocup));
-        strncpy(info.statePlaying, json_value[k++].asString().c_str(), sizeof(info.statePlaying));
-        strncpy(info.stateSearch, json_value[k++].asString().c_str(), sizeof(info.stateSearch));
-        strncpy(info.hardwareWarnings, json_value[k++].asString().c_str(), sizeof(info.hardwareWarnings));
-        
-        // Hour
-        info.hour = json_value[k++].asInt();
-        info.min = json_value[k++].asInt();
-        info.sec = json_value[k++].asInt();
-        
-        // Obstacles
-        info.obstaclesRadius = json_value[k++].asFloat();
-        info.nbObstacles = 0;
-        for (auto obstacle : json_value[k++]) {
-            info.obstacles[info.nbObstacles][0] = obstacle[0].asFloat();
-            info.obstacles[info.nbObstacles][1] = obstacle[1].asFloat();
-            info.nbObstacles++;
-        }
-        
-        info.timestamp = json_value[k++].asFloat();
-        
-        info.goalKeeper = json_value[k++].asBool();
-    } else {
-        std::cerr << "TeamPlayInfo::fromJson bad array size!" << std::endl;    
-    }
-}
-                    
-Json::Value teamPlayToJson(const TeamPlayInfo &info)
-{
-    Json::Value json(Json::arrayValue);
-    
     // Robot id and state
-    json.append(info.id);
-    json.append(info.state);
-    
+    info.id = json_value[k++].asInt();
+    info.state = (TeamPlayState)json_value[k++].asInt();
+
     // Ball
-    json.append(info.ballX);
-    json.append(info.ballY);
-    json.append(info.ballQ);
-    json.append(info.ballOk);
-    json.append(info.ballVelX);
-    json.append(info.ballVelY);
-    
+    info.ballX = json_value[k++].asFloat();
+    info.ballY = json_value[k++].asFloat();
+    info.ballQ = json_value[k++].asFloat();
+    info.ballOk = json_value[k++].asBool();
+    info.ballVelX = json_value[k++].asFloat();
+    info.ballVelY = json_value[k++].asFloat();
+
     // Field
-    json.append(info.fieldX);
-    json.append(info.fieldY);
-    json.append(info.fieldYaw);
-    json.append(info.fieldQ);
-    json.append(info.fieldConsistency);
-    json.append(info.fieldOk);
-    
+    info.fieldX = json_value[k++].asFloat();
+    info.fieldY = json_value[k++].asFloat();
+    info.fieldYaw = json_value[k++].asFloat();
+    info.fieldQ = json_value[k++].asFloat();
+    info.fieldConsistency = json_value[k++].asFloat();
+    info.fieldOk = json_value[k++].asBool();
+
     // Placing data
-    json.append(info.placing);
-    json.append(info.targetX);
-    json.append(info.targetY);
-    json.append(info.localTargetX);
-    json.append(info.localTargetY);
-    
-    // Kick info
-    json.append(info.ballTargetX);
-    json.append(info.ballTargetY);
-    json.append(info.timeSinceLastKick);
-    
+    info.placing = json_value[k++].asBool();
+    info.targetX = json_value[k++].asFloat();
+    info.targetY = json_value[k++].asFloat();
+    info.localTargetX = json_value[k++].asFloat();
+    info.localTargetY = json_value[k++].asFloat();
+
+    // Kick
+    info.ballTargetX = json_value[k++].asFloat();
+    info.ballTargetY = json_value[k++].asFloat();
+    info.timeSinceLastKick = json_value[k++].asFloat();
+
     // States
-    json.append(info.stateReferee);
-    json.append(info.stateRobocup);
-    json.append(info.statePlaying);
-    json.append(info.stateSearch);
-    json.append(info.hardwareWarnings);
-    
-    // Timing
-    json.append(info.hour);
-    json.append(info.min);
-    json.append(info.sec);
-    
+    strncpy(info.stateReferee, json_value[k++].asString().c_str(), sizeof(info.stateReferee));
+    strncpy(info.stateRobocup, json_value[k++].asString().c_str(), sizeof(info.stateRobocup));
+    strncpy(info.statePlaying, json_value[k++].asString().c_str(), sizeof(info.statePlaying));
+    strncpy(info.stateSearch, json_value[k++].asString().c_str(), sizeof(info.stateSearch));
+    strncpy(info.hardwareWarnings, json_value[k++].asString().c_str(), sizeof(info.hardwareWarnings));
+
+    // Hour
+    info.hour = json_value[k++].asInt();
+    info.min = json_value[k++].asInt();
+    info.sec = json_value[k++].asInt();
+
     // Obstacles
-    json.append(info.obstaclesRadius);
-    Json::Value obstacles = Json::Value(Json::arrayValue);
-    for (int k=0; k<info.nbObstacles; k++) {
-        Json::Value obstacle(Json::arrayValue);
-        obstacle[0] = info.obstacles[k][0];
-        obstacle[1] = info.obstacles[k][1];
-        obstacles.append(obstacle);
+    info.obstaclesRadius = json_value[k++].asFloat();
+    info.nbObstacles = 0;
+    for (auto obstacle : json_value[k++])
+    {
+      info.obstacles[info.nbObstacles][0] = obstacle[0].asFloat();
+      info.obstacles[info.nbObstacles][1] = obstacle[1].asFloat();
+      info.nbObstacles++;
     }
-    json.append(obstacles);
-    
-    // Timestamp
-    json.append(info.timestamp);
-    
-    // Is the robot a goal keeper ?
-    json.append(info.goalKeeper);
-    
-    return json;
+
+    info.timestamp = json_value[k++].asFloat();
+
+    info.goalKeeper = json_value[k++].asBool();
+  }
+  else
+  {
+    std::cerr << "TeamPlayInfo::fromJson bad array size!" << std::endl;
+  }
+}
+
+Json::Value teamPlayToJson(const TeamPlayInfo& info)
+{
+  Json::Value json(Json::arrayValue);
+
+  // Robot id and state
+  json.append(info.id);
+  json.append(info.state);
+
+  // Ball
+  json.append(info.ballX);
+  json.append(info.ballY);
+  json.append(info.ballQ);
+  json.append(info.ballOk);
+  json.append(info.ballVelX);
+  json.append(info.ballVelY);
+
+  // Field
+  json.append(info.fieldX);
+  json.append(info.fieldY);
+  json.append(info.fieldYaw);
+  json.append(info.fieldQ);
+  json.append(info.fieldConsistency);
+  json.append(info.fieldOk);
+
+  // Placing data
+  json.append(info.placing);
+  json.append(info.targetX);
+  json.append(info.targetY);
+  json.append(info.localTargetX);
+  json.append(info.localTargetY);
+
+  // Kick info
+  json.append(info.ballTargetX);
+  json.append(info.ballTargetY);
+  json.append(info.timeSinceLastKick);
+
+  // States
+  json.append(info.stateReferee);
+  json.append(info.stateRobocup);
+  json.append(info.statePlaying);
+  json.append(info.stateSearch);
+  json.append(info.hardwareWarnings);
+
+  // Timing
+  json.append(info.hour);
+  json.append(info.min);
+  json.append(info.sec);
+
+  // Obstacles
+  json.append(info.obstaclesRadius);
+  Json::Value obstacles = Json::Value(Json::arrayValue);
+  for (int k = 0; k < info.nbObstacles; k++)
+  {
+    Json::Value obstacle(Json::arrayValue);
+    obstacle[0] = info.obstacles[k][0];
+    obstacle[1] = info.obstacles[k][1];
+    obstacles.append(obstacle);
+  }
+  json.append(obstacles);
+
+  // Timestamp
+  json.append(info.timestamp);
+
+  // Is the robot a goal keeper ?
+  json.append(info.goalKeeper);
+
+  return json;
 }
 
 float TeamPlayInfo::getAge() const
 {
-    return TimeStamp::now().getTimeMS() - timestamp;
+  return TimeStamp::now().getTimeMS() - timestamp;
 }
 
 bool TeamPlayInfo::isPenalized() const
@@ -156,18 +160,18 @@ bool TeamPlayInfo::isPenalized() const
 
 bool TeamPlayInfo::isOutdated() const
 {
-    //Outdated after 3 seconds
-    return (getAge() > 3000);
+  // Outdated after 3 seconds
+  return (getAge() > 3000);
 }
 
 float TeamPlayInfo::getBallDistance() const
 {
-    return sqrt(ballX*ballX + ballY*ballY);
+  return sqrt(ballX * ballX + ballY * ballY);
 }
 
 float TeamPlayInfo::getBallAzimuth() const
 {
-    return atan2(ballY, ballX);
+  return atan2(ballY, ballX);
 }
 
 Eigen::Vector2d TeamPlayInfo::getBallInField() const
@@ -178,127 +182,143 @@ Eigen::Vector2d TeamPlayInfo::getBallInField() const
   return ball_in_self;
 }
 
-CaptainInfo::CaptainInfo()
-: id(-1)
+CaptainInfo::CaptainInfo() : id(-1)
 {
-    for (int k=0; k<CAPTAIN_MAX_ID; k++) {
-        order[k] = SearchBall;
-        robotTarget[k][0] = 0;
-        robotTarget[k][1] = 0;
-        robotTarget[k][2] = 0;
-    }
-    common_ball.nbRobots = 0;
-    common_ball.x = 0;
-    common_ball.y = 0;
-    nb_opponents = 0;
-    for (int k=0; k<MAX_OPPONENTS; k++) {
-      common_opponents[k].consensusStrength = 0;
-      common_opponents[k].x = 0;
-      common_opponents[k].y= 0;
-    }
+  for (int k = 0; k < CAPTAIN_MAX_ID; k++)
+  {
+    order[k] = SearchBall;
+    robotTarget[k][0] = 0;
+    robotTarget[k][1] = 0;
+    robotTarget[k][2] = 0;
+  }
+  common_ball.nbRobots = 0;
+  common_ball.x = 0;
+  common_ball.y = 0;
+  nb_opponents = 0;
+  for (int k = 0; k < MAX_OPPONENTS; k++)
+  {
+    common_opponents[k].consensusStrength = 0;
+    common_opponents[k].x = 0;
+    common_opponents[k].y = 0;
+  }
 }
 
 float CaptainInfo::getAge() const
 {
-    return TimeStamp::now().getTimeMS() - timestamp;
+  return TimeStamp::now().getTimeMS() - timestamp;
 }
 
 int CaptainInfo::getHandler() const
 {
-  for (int robot_id = 0; robot_id < CAPTAIN_MAX_ID; robot_id++) {
-    if (order[robot_id] == CaptainOrder::HandleBall) {
+  for (int robot_id = 0; robot_id < CAPTAIN_MAX_ID; robot_id++)
+  {
+    if (order[robot_id] == CaptainOrder::HandleBall)
+    {
       return robot_id + 1;
     }
   }
   return -1;
 }
 
-void captainFromJson(CaptainInfo &info, const Json::Value & json_value)
+void captainFromJson(CaptainInfo& info, const Json::Value& json_value)
 {
-    if (json_value.size() == 8) {
-        int k = 0;
-        
-        info.id = json_value[k++].asInt();
-        
-        auto targets = json_value[k++];
-        for (int n=0; n<CAPTAIN_MAX_ID; n++) {
-            info.robotTarget[n][0] = targets[n][0].asFloat();
-            info.robotTarget[n][1] = targets[n][1].asFloat();
-            info.robotTarget[n][2] = targets[n][2].asFloat();
-        }
-        
-        auto orders = json_value[k++];
-        for (int n=0; n<CAPTAIN_MAX_ID; n++) {
-            info.order[n] = (CaptainOrder)orders[n].asInt();
-        }
-        
-        info.timestamp = json_value[k++].asFloat();
+  if (json_value.size() == 8)
+  {
+    int k = 0;
 
-        info.common_ball.nbRobots = json_value[k++].asInt();
-        info.common_ball.x = json_value[k++].asFloat();
-        info.common_ball.y = json_value[k++].asFloat();
+    info.id = json_value[k++].asInt();
 
-        const Json::Value & common_opponents_json = json_value[k++];
-        if (!common_opponents_json.isArray()) {
-          throw std::logic_error("Common opponents is not an array");
-          info.nb_opponents = common_opponents_json.size();
-          for (int opp_id = 0; opp_id < MAX_OPPONENTS; opp_id++) {
-            if (opp_id < info.nb_opponents) {
-              const Json::Value & opp_json = common_opponents_json[opp_id];
-              info.common_opponents[opp_id].consensusStrength = opp_json[opp_id].asInt();
-              info.common_opponents[opp_id].x = opp_json[opp_id].asFloat();
-              info.common_opponents[opp_id].y = opp_json[opp_id].asFloat();
-            } else {
-              info.common_opponents[opp_id].consensusStrength = 0;
-              info.common_opponents[opp_id].x = 0;
-              info.common_opponents[opp_id].y = 0;
-            }
-          }
-        }
-    } else {
-        std::cerr << "CaptainInfo bad json size" << std::endl;    
+    auto targets = json_value[k++];
+    for (int n = 0; n < CAPTAIN_MAX_ID; n++)
+    {
+      info.robotTarget[n][0] = targets[n][0].asFloat();
+      info.robotTarget[n][1] = targets[n][1].asFloat();
+      info.robotTarget[n][2] = targets[n][2].asFloat();
     }
+
+    auto orders = json_value[k++];
+    for (int n = 0; n < CAPTAIN_MAX_ID; n++)
+    {
+      info.order[n] = (CaptainOrder)orders[n].asInt();
+    }
+
+    info.timestamp = json_value[k++].asFloat();
+
+    info.common_ball.nbRobots = json_value[k++].asInt();
+    info.common_ball.x = json_value[k++].asFloat();
+    info.common_ball.y = json_value[k++].asFloat();
+
+    const Json::Value& common_opponents_json = json_value[k++];
+    if (!common_opponents_json.isArray())
+    {
+      throw std::logic_error("Common opponents is not an array");
+      info.nb_opponents = common_opponents_json.size();
+      for (int opp_id = 0; opp_id < MAX_OPPONENTS; opp_id++)
+      {
+        if (opp_id < info.nb_opponents)
+        {
+          const Json::Value& opp_json = common_opponents_json[opp_id];
+          info.common_opponents[opp_id].consensusStrength = opp_json[opp_id].asInt();
+          info.common_opponents[opp_id].x = opp_json[opp_id].asFloat();
+          info.common_opponents[opp_id].y = opp_json[opp_id].asFloat();
+        }
+        else
+        {
+          info.common_opponents[opp_id].consensusStrength = 0;
+          info.common_opponents[opp_id].x = 0;
+          info.common_opponents[opp_id].y = 0;
+        }
+      }
+    }
+  }
+  else
+  {
+    std::cerr << "CaptainInfo bad json size" << std::endl;
+  }
 }
-                    
-Json::Value captainToJson(const CaptainInfo &info)
+
+Json::Value captainToJson(const CaptainInfo& info)
 {
-    Json::Value json;
-    
-    json.append(info.id);
-    
-    Json::Value targets(Json::arrayValue);
-    for (int k=0; k<CAPTAIN_MAX_ID; k++) {
-        Json::Value target(Json::arrayValue);
-        target.append(info.robotTarget[k][0]);
-        target.append(info.robotTarget[k][1]);
-        target.append(info.robotTarget[k][2]);
-        targets.append(target);
-    }
-    json.append(targets);
-    
-    Json::Value orders(Json::arrayValue);
-    for (int k=0; k<CAPTAIN_MAX_ID; k++) {
-        orders.append(info.order[k]);
-    }
-    json.append(orders);
-    
-    json.append(info.timestamp);
+  Json::Value json;
 
-    json.append(info.common_ball.nbRobots);
-    json.append(info.common_ball.x);
-    json.append(info.common_ball.y);
+  json.append(info.id);
 
-    Json::Value opponents(Json::arrayValue);
-    for (int k = 0; k < info.nb_opponents; k++) {
-      Json::Value opp(Json::arrayValue);
-      opp.append(info.common_opponents[k].consensusStrength);
-      opp.append(info.common_opponents[k].x);
-      opp.append(info.common_opponents[k].y);
-      opponents.append(opp);
-    }
-    json.append(opponents);
-    
-    return json;
+  Json::Value targets(Json::arrayValue);
+  for (int k = 0; k < CAPTAIN_MAX_ID; k++)
+  {
+    Json::Value target(Json::arrayValue);
+    target.append(info.robotTarget[k][0]);
+    target.append(info.robotTarget[k][1]);
+    target.append(info.robotTarget[k][2]);
+    targets.append(target);
+  }
+  json.append(targets);
+
+  Json::Value orders(Json::arrayValue);
+  for (int k = 0; k < CAPTAIN_MAX_ID; k++)
+  {
+    orders.append(info.order[k]);
+  }
+  json.append(orders);
+
+  json.append(info.timestamp);
+
+  json.append(info.common_ball.nbRobots);
+  json.append(info.common_ball.x);
+  json.append(info.common_ball.y);
+
+  Json::Value opponents(Json::arrayValue);
+  for (int k = 0; k < info.nb_opponents; k++)
+  {
+    Json::Value opp(Json::arrayValue);
+    opp.append(info.common_opponents[k].consensusStrength);
+    opp.append(info.common_opponents[k].x);
+    opp.append(info.common_opponents[k].y);
+    opponents.append(opp);
+  }
+  json.append(opponents);
+
+  return json;
 }
 
-}
+}  // namespace rhoban_team_play
